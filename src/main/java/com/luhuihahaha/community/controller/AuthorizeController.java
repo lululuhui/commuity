0e4c764e23911f09a6f23540cd95326bd710bbb4
luhuihahaha <2247192213@qq.com>
2020-02-10 10:27:22 +0800

    add others file

--- src/main/java/com/luhuihahaha/community/controller/AuthorizeController.java
+++ src/main/java/com/luhuihahaha/community/controller/AuthorizeController.java
@@ -1,4 +1,42 @@
 package com.luhuihahaha.community.controller;
 
+import com.luhuihahaha.community.dto.AccessTokenDTO;
+import com.luhuihahaha.community.dto.GithubUser;
+import com.luhuihahaha.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.clent.id}")
+    private String githubClientId;
+
+    @Value("${github.client.secret}")
+    private String githubClientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String githubRedirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name = "state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(githubClientId);
+        accessTokenDTO.setClient_secret(githubClientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(githubRedirectUri);
+        accessTokenDTO.setState(state);
+        String accseeToken = githubProvider.getAccseeToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accseeToken);
+        System.out.println(user.getLogin());
+        return "index";
+    }
+
 }
--- src/main/java/com/luhuihahaha/community/controller/GreetingController.java
+++ src/main/java/com/luhuihahaha/community/controller/GreetingController.java
@@ -1,21 +1,16 @@
 package com.luhuihahaha.community.controller;
 
 
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
+@Controller
 public class GreetingController {
 
-    @GetMapping("/greet")
-    @ResponseBody
-    public String greeting(@RequestParam(name = "name") String name, Model model){
+    @GetMapping("/")
+    public String index(){ return "index";}
 
-        model.addAttribute("name",name);
-        return "hello";
 
-    }
 
 }
